2681030ca8b4fe791b21c6850964340e6074be36
Voxianne <athina358@gmail.com>
2025-01-25 18:04:56 +0200

    ok

--- frontend/contractObjectsWeb3.cjs
+++ frontend/contractObjectsWeb3.cjs
@@ -8,7 +8,7 @@ const taskInitContractAddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
 const taskSelectAddress = "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0";
 const rewardSysAddress = "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9";
 
- const { abi: userAbi} = require("../artifacts/contracts/UserInformation.sol/UserInformation.json");
+const { abi: userAbi} = require("../artifacts/contracts/UserInformation.sol/UserInformation.json");
 const { abi: taskInitAbi} = require("../artifacts/contracts/Task_Initialization.sol/Task_Initialization.json");
 const { abi: taskSelectAbi} = require("../artifacts/contracts/Task_Selection.sol/Task_Selection.json");
 const { abi: rewardSysAbi} = require("../artifacts/contracts/Reward_Penalty_System.sol/Reward_Penalty_System.json");
