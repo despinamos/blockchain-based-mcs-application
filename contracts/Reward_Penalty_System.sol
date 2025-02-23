b2c6d07b4a08589f142697c60f364f56ba2e9cf5
Voxianne <athina358@gmail.com>
2025-02-23 14:51:19 +0200

    οκ

--- contracts/Reward_Penalty_System.sol
+++ contracts/Reward_Penalty_System.sol
@@ -14,9 +14,11 @@ import './Task_Initialization.sol';
 contract Reward_Penalty_System is Task_Initialization{
 
 
-    function Reward_Process(uint256 user_ID) public {
-        //pass
-        Reputation_Score_Update( user_ID);
+    function Reward_Process(uint256 user_ID) public payable {
+        
+        //Reward = 1 - Penalty = 0
+        // transfer tasks[_unique_taskid].reward to users[user_ID].user_address wallet
+        Reputation_Score_Update( user_ID, 1, 0);
     }
 
     //We tested the quality of the data and depending of how insufficient the data is, we give a greater the penalty
@@ -24,16 +26,18 @@ contract Reward_Penalty_System is Task_Initialization{
         //Here we take the given data and we check how below of the sufficient quality percentage they are. The lower they are, the higher the penalty percentage.
         //pass
         //Emit task ID has been cancelled ...And call function for Reputation
-       Reputation_Score_Update(user_ID);
+
+        //Amazing!! We have a penalty of 1 and a reward of 0 for the user who has been penalized for the task he/she has cancelled  :(
+       Reputation_Score_Update(user_ID, 0, 1);
 
     }
 
 
     
     //We also add to the reputation score
-    function Reputation_Score_Update(uint256 user_ID) public {
+    function Reputation_Score_Update(uint256 user_ID, uint256 reward, uint256 penalty) public {
         //Calculating the users's reputation by using the cancelled and completed requests
-            uint256  rep = users[user_ID].reputation - users[user_ID].cancelled_tasks * 5 + users[user_ID].completed_tasks * 10;
+            uint256  rep = users[user_ID].reputation - users[user_ID].cancelled_tasks * 5 + users[user_ID].completed_tasks * 10 + reward - penalty;
             users[user_ID].reputation = rep;
     }
 }
