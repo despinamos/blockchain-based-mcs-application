e9c6466418fa2514b9abd96c6e604e71814dd0c6
Voxianne <athina358@gmail.com>
2025-05-24 19:28:04 +0300

    Pinana Saviour

--- scripts/ipfsConnectionPinata.js
+++ scripts/ipfsConnectionPinata.js
@@ -1,14 +1,16 @@
 import { PinataSDK } from "pinata";
+import 'dotenv/config';
 
 const pinata = new PinataSDK({
-  pinataJwt: "PINATA_JWT",
-  pinataGateway: "white-tragic-rook-965.mypinata.cloud",
+  pinataJwt: process.env.PINATA_JWT,
+ pinataGateway: process.env.PINATA_GATEWAY,
 });
 
-// Workers will upload the file to IPFS, then take their cid and submit it to blockchain
+
+// // Workers will upload the file to IPFS, then take their cid and submit it to blockchain
 async function uploadFile() {
   try {
-    const file = new File(["hello world!"], "hello.txt", { type: "text/plain" });
+    const file = new File(["I have a feeling that somebody watching meeeeee..."], "despair.txt", { type: "text/plain" });
     const upload = await pinata.upload.public.file(file);
     console.log(upload);
   } catch (error) {
@@ -16,20 +18,21 @@ async function uploadFile() {
   }
 }
 
-// When a Requester wants to review the data sent by the Workers, they will get the file from IPFS using the
-// data hashes sent by the Workers (getDataHash).
-// Data quality evaluation should only be plausible after a Task has been set as Reserved.
-// Data quality evaluation is executed off-chain (js backend) and then the results are posted on-chain.
-async function getFile() {
-  try {
-    const data = await pinata.gateways.public.get("bafkreibm6jg3ux5qumhcn2b3flc3tyu6dmlb4xa7u5bf44yegnrjhc4yeq");
+// // When a Requester wants to review the data sent by the Workers, they will get the file from IPFS using the
+// // data hashes sent by the Workers (getDataHash).
+// // Data quality evaluation should only be plausible after a Task has been set as Reserved.
+// // Data quality evaluation is executed off-chain (js backend) and then the results are posted on-chain.
+async function getFile(cid) {
+  try { 
+    const data = await pinata.gateways.public.get(cid);
     console.log(data)
 
-    const url = await pinata.gateways.convert(
-      "bafkreib4pqtikzdjlj4zigobmd63lig7u6oxlug24snlr6atjlmlza45dq"
-    )
-    console.log(url)
+    const url = `${process.env.PINATA_GATEWAY}/ipfs/${cid}`;
+    console.log("Access URL:", url);
   } catch (error) {
     console.log(error);
   }
 }
+
+uploadFile();
+getFile("bafkreid2kwk3yoepscor4yckwngy5zuacfswkyfujs5tv6ls5jxxfk63gq");
