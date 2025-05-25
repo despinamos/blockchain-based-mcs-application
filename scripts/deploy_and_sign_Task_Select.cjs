46830a82c264e05d4af2fbe1c3c0e49c5e2a9772
Voxianne <athina358@gmail.com>
2025-05-25 22:06:27 +0300

    Adaptation to new situations

--- scripts/deploy_and_sign_Task_Select.cjs
+++ scripts/deploy_and_sign_Task_Select.cjs
@@ -9,7 +9,7 @@ if(typeof window !== "undefined" && window.ethereum) {
   provider = new ethers.providers.JsonRpcProvider(url);
 }
 
-const privateKey = "0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80";
+const privateKey = "ACCOUNT_PRIVATE_KEY";
 const wallet = new ethers.Wallet(privateKey, provider);
 
 const { abi: taskSelectAbi } = require("../artifacts/contracts/Task_Selection.sol/Task_Selection.json")
@@ -38,14 +38,14 @@ const deployTaskSelection = async () => {
 const iface = new ethers.utils.Interface(taskSelectAbi);
 
 // User Information functions
-const dataSetUserInfo = iface.encodeFunctionData("setUser_Information", ["Anna", "Kozani"]);
+const dataSetUserInfo = iface.encodeFunctionData("setUser_Information", ["Anna", "Athens"]);
 //console.log("Set User Info data:", dataSetUserInfo);
 
 const dataGetUserInfo = iface.encodeFunctionData("getUserInformation", [1]);
 //console.log("Get User Info data: ", dataGetUserInfo);
 
 // Task Initialization functions
-const dataSetTaskInfo = iface.encodeFunctionData("setTask_Information", ["Play", "Play Persona", 3, 3, 100]);
+const dataSetTaskInfo = iface.encodeFunctionData("setTask_Information", ["Weather", "Check Weather", 3, 5, 100]);
 //console.log("Set User Info data:", dataSetUserInfo);
 
 const dataGetTaskInfo = iface.encodeFunctionData("getTaskInformation", [1]);
@@ -60,7 +60,7 @@ const dataGetTaskInfo = iface.encodeFunctionData("getTaskInformation", [1]);
 
 // Task Select functions
 const dataTableTaskRequester = iface.encodeFunctionData("Table_Task_Requester", );
-//console.log("Table Task Requester, ", dataTableTaskRequester);
+console.log("Table Task Requester, ", dataTableTaskRequester);
 
 const dataTableTaskWorker = iface.encodeFunctionData("Table_Task_Worker", );
 console.log("Table Task Worker, ", dataTableTaskWorker);
@@ -83,8 +83,8 @@ async function send() {
 
   // const response = await wallet.signTransaction(tx);
   // console.log("Raw Transaction: ", response);
-  const encodedResult = "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000035204e616d653a20506c6179202d20496e666f726d6174696f6e3a20506c617920506572736f6e61202d205265776172643a203130300000000000000000000000";
-  const decoded = iface.decodeFunctionResult("Table_Task_Requester", encodedResult);
+  const encodedResult = "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000039204e616d653a2057656174686572202d20496e666f726d6174696f6e3a20436865636b2057656174686572202d205265776172643a2031303000000000000000";
+  const decoded = iface.decodeFunctionResult("Table_Task_Worker", encodedResult);
   console.log(decoded);
 }
 
