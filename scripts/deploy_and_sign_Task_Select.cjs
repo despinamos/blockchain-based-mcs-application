5ba95b5e3cd91bb01f8859c7ecaf5838b7d215fe
D. Moschokarfi <despinamx16@gmail.com>
2025-06-25 14:56:14 +0300

    changes

--- scripts/deploy_and_sign_Task_Select.cjs
+++ scripts/deploy_and_sign_Task_Select.cjs
@@ -45,47 +45,46 @@ const dataGetUserInfo = iface.encodeFunctionData("getUserInformation", [1]);
 //console.log("Get User Info data: ", dataGetUserInfo);
 
 // Task Initialization functions
-const dataSetTaskInfo = iface.encodeFunctionData("setTask_Information", ["Weather", "Check Weather", 3, 5, 100]);
+const dataSetTaskInfo = iface.encodeFunctionData("setTask_Information", ["Weather", "Check Weather", 4, 100]);
 //console.log("Set User Info data:", dataSetUserInfo);
 
 const dataGetTaskInfo = iface.encodeFunctionData("getTaskInformation", [1]);
 //console.log("Get Task Info data: ", dataGetTaskInfo);
 
-//Reward Penalty System functions
-// const dataRewardProcess = iface.encodeFunctionData("Reward_Process", userId, taskId);
-
-// const dataPenaltyProcess = iface.encodeFunctionData("Reward_Process", userId);
-
-// const dataReputationScoreUpdate = iface.encodeFunctionData("Reward_Process", userId, reward, penalty);
-
 // Task Select functions
 const dataTableTaskRequester = iface.encodeFunctionData("Table_Task_Requester", );
-console.log("Table Task Requester, ", dataTableTaskRequester);
+//console.log("Table Task Requester, ", dataTableTaskRequester);
 
 const dataTableTaskWorker = iface.encodeFunctionData("Table_Task_Worker", );
-console.log("Table Task Worker, ", dataTableTaskWorker);
+//console.log("Table Task Worker, ", dataTableTaskWorker);
+
+const dataTaskCancelledByWorker = iface.encodeFunctionData("Task_Cancelled_By_Worker", [1]);
 
 const dataSelectWorker = iface.encodeFunctionData("Select_Worker", );
+
+const dataResultSetByRequester = iface.encodeFunctionData("Result_Set_by_Requester", [1, 3, 1]);
+
+const dataResultAfterCalculation = iface.encodeFunctionData("Result_after_Calculation", [1]);
 
 async function send() {
   // const taskSelectConAddress = await deployTaskSelection();
   // console.log("Task Selection contract address: ", taskSelectConAddress)
-  // const nonce = await provider.getTransactionCount(wallet.address);
-  // const gasPrice = await provider.getGasPrice();
+  const nonce = await provider.getTransactionCount(wallet.address);
+  const gasPrice = await provider.getGasPrice();
 
-  // const tx = {
-  //   to: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
-  //   data: dataSelectWorker,
-  //   nonce,
-  //   gasLimit: 10000000,
-  //   gasPrice
-  // }
+  const tx = {
+    to: "0x5FbDB2315678afecb367f032d93F642f64180aa3",
+    data: dataResultAfterCalculation,
+    nonce,
+    gasLimit: 10000000,
+    gasPrice
+  }
 
-  // const response = await wallet.signTransaction(tx);
-  // console.log("Raw Transaction: ", response);
-  const encodedResult = "0x0000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000000100000000000000000000000000000000000000000000000000000000000000200000000000000000000000000000000000000000000000000000000000000039204e616d653a2057656174686572202d20496e666f726d6174696f6e3a20436865636b2057656174686572202d205265776172643a2031303000000000000000";
-  const decoded = iface.decodeFunctionResult("Table_Task_Worker", encodedResult);
-  console.log(decoded);
+  const response = await wallet.signTransaction(tx);
+  console.log("Raw Transaction: ", response);
+  // const encodedResult = "0x000000000000000000000000000000000000000000000000000000000000002000000000000000000000000000000000000000000000000000000000000000010000000000000000000000000000000000000000000000000000000000000020000000000000000000000000000000000000000000000000000000000000002257656174686572202d20436865636b2057656174686572202d205265736572766564000000000000000000000000000000000000000000000000000000000000";
+  // const decoded = iface.decodeFunctionResult("Table_Task_Requester", encodedResult);
+  // console.log(decoded);
 }
 
 send();
